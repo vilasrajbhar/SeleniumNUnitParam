eb400b14d2349b4c5dd90edbb08827088c9c2ea3
Vilas Rajbhar <vilas.rajbhar89@gmail.com>
2020-05-15 21:54:04 +0530

    ReqNo1: New Selenium Csharp Nunit Project Added.

---
+++ BrowserTest.cs
@@ -0,0 +1,51 @@
+﻿
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumNUnitParam
+{
+    [TestFixture]
+    [Parallelizable]
+    public class FireFoxTesting : Hooks
+    {
+        public FireFoxTesting(): base(BrowerType.Chrome)
+        {
+
+        }
+
+        [Test]
+        public void GoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+            System.Threading.Thread.Sleep(5000);
+            Driver.FindElement(By.Name("btnK")).Click();
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                            "The text selenium doest not exist");
+
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable]
+    public class ChromeTesting : Hooks
+    {
+        public ChromeTesting() : base(BrowerType.Chrome)
+        {
+
+        }
+
+        [Test]
+        public void ExecuteAutomationTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("ExecuteAutomation");
+            System.Threading.Thread.Sleep(5000);
+            Driver.FindElement(By.Name("btnK")).Click();
+            Assert.That(Driver.PageSource.Contains("ExecuteAutomation"), Is.EqualTo(true),
+                                            "The text ExecuteAutomation doest not exist");
+
+        }
+    }
+}
---
+++ Hooks.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumNUnitParam
+{
+    //Enum for browserType
+    public enum BrowerType
+    {
+        Chrome,
+        Firefox,
+        IE
+    }
+
+    [TestFixture]
+    public class Hooks : Base
+    {
+        private BrowerType _browserType;
+
+        public Hooks(BrowerType brower)
+        {
+            _browserType = brower;
+        }
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            ChooseDriverInstance(_browserType);
+        }
+
+        private void ChooseDriverInstance(BrowerType browserType)
+        {
+            if (browserType == BrowerType.Chrome)
+                Driver = new ChromeDriver();
+            else if (browserType == BrowerType.Firefox)
+            {
+                Driver = new FirefoxDriver();
+            }
+            else if(browserType == BrowerType.IE)
+            {
+                Driver = new InternetExplorerDriver();
+            }
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            Driver.Quit();
+        }
+    }
+}
